d5e0eba5adfe98fbbca26f937b2e408d259d83ab
Anatoliy Danilyuk <anatolydanilyuk2002@gmail.com>
2024-01-09 23:09:05 +0200

    s

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
+import ClientComponent from "./components/client-component";
+import ServerComponent from "./components/server-component";
 import StatusLabel, { Status } from "./components/status-label";
 
 export default function Home() {
   return (
     <main>
       <h1 className="text-xl">Home page</h1>
-      <StatusLabel status={Status.Active}>Active</StatusLabel>
-      <StatusLabel status={Status.NotActive}>Not Active</StatusLabel>
-      <StatusLabel status={Status.Pending}>Pending</StatusLabel>
-      <StatusLabel status={Status.Suspended}>Suspended</StatusLabel>
+      <StatusLabel status={Status.Active} disabled={false}>Active</StatusLabel>
+      <StatusLabel status={Status.NotActive} disabled={false}>Not Active</StatusLabel>
+      <StatusLabel status={Status.Pending} disabled={true}>Pending</StatusLabel>
+      <StatusLabel status={Status.Suspended} disabled={true}>Suspended</StatusLabel>
+      <ServerComponent/>
+      <ClientComponent/>
     </main>
   );
 }
